603af1f2b098a477af281b4f425688099053559d
İrem KARA <aylakmadam99@gmail.com>
2023-03-08 17:03:08 +0300

    time and date set

--- app.js
+++ app.js
@@ -7,17 +7,24 @@ const countryElement = document.getElementById("country");
 const futureWeatherElement = document.getElementById ("future-weather");
 const todayTempElement = document.getElementById("today-temp");
 
+const days = ["Pazartesi","Salı","Çarşamba","Perşembe","Cuma","Cumartesi","Pazar"];
+const months = ["Ocak","Şubat","Mart", " Nisan" , "Mayıs" ,"Haziran" , "Temmuz" , "Ağustos",
+"Eylül" , "Ekim" ,"Kasım" , "Aralık"];
+
 setInterval (() => {
     const time =new Date();
     const month = time.getMonth();
     const date = time.getDate();
     const day = time.getDay();
     const hour = time.getHours();
-    const hoursIn12HrFormat = hour >= 13 ? hour %12: hour
+    const hoursIn12HrFormat = hour >= 13 ? hour %12: hour;
     const minutes = time.getMinutes();
     const year = time.getFullYear();
     const ampm = hour >=12 ? 'PM' : 'AM'
 
-    //timeElement.innerHTML = hoursIn12HrFormat + ":" + minutes+
+    timeElement.innerHTML = hoursIn12HrFormat + ":" + minutes+ `<span id="am-pm">${ampm}</span>`;
+    dateElement.innerHTML = days[day] + "," + " " +date + months[month] ;
     
-},1000)
+   
+
+})

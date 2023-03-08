a1fadb36c4868fd868ff8c1039e60899630f7ab7
İrem KARA <aylakmadam99@gmail.com>
2023-03-08 15:41:00 +0300

    add js full time and date

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
 const timeElement = document.getElementById("time");
 const dateElement = document.getElementById("date");
+const yearElement =document.getElementById("year");
 const weatherItemsElement = document.getElementById("weather-items");
 const timeDayElement = document.getElementById("time-day");
 const countryElement = document.getElementById("country");
 const futureWeatherElement = document.getElementById ("future-weather");
 const todayTempElement = document.getElementById("today-temp");
+
+setInterval (() => {
+    const time =new Date();
+    const month = time.getMonth();
+    const date = time.getDate();
+    const day = time.getDay();
+    const hour = time.getHours();
+    const hoursIn12HrFormat = hour >= 13 ? hour %12: hour
+    const minutes = time.getMinutes();
+    const year = time.getFullYear();
+    const ampm = hour >=12 ? 'PM' : 'AM'
+
+    //timeElement.innerHTML = hoursIn12HrFormat + ":" + minutes+
+    
+},1000)

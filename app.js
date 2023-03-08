0d9eda0fd75c7c96d3dcf97513ca072b04328fe0
İrem KARA <aylakmadam99@gmail.com>
2023-03-08 15:05:45 +0300

    adding eventlistener inside javascript.

---
+++ app.js
@@ -0,0 +1,7 @@
+const timeElement = document.getElementById("time");
+const dateElement = document.getElementById("date");
+const weatherItemsElement = document.getElementById("weather-items");
+const timeDayElement = document.getElementById("time-day");
+const countryElement = document.getElementById("country");
+const futureWeatherElement = document.getElementById ("future-weather");
+const todayTempElement = document.getElementById("today-temp");

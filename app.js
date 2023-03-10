0ce46b1bb7e948343362fdf7ed9f27f59c8db4cd
İrem KARA <aylakmadam99@gmail.com>
2023-03-10 17:13:45 +0300

    fatal weather items : nem, rüzgar hızı,basınç , gün doğuşu, gün batımı

--- app.js
+++ app.js
@@ -15,6 +15,27 @@ const todayTempElement = document.getElementById("today-temp");
 const formElement = document.getElementById("search")
 
 
+
+
+const days = ["Pazar", "Pazartesi", "Salı", "Çarşamba", "Perşembe", "Cuma", "Cumartesi"];
+const months = ["Ocak", "Şubat", "Mart", " Nisan", "Mayıs", "Haziran", "Temmuz", "Ağustos","Eylül", "Ekim", "Kasım", "Aralık"];
+
+setInterval(() => {
+    const time = new Date();
+    const month = time.getMonth();
+    const date = time.getDate();
+    const day = time.getDay();
+    const hour = time.getHours();
+    const hoursIn12HrFormat = hour >= 13 ? hour % 12 : hour;
+    const minutes = time.getMinutes();
+    const year = time.getFullYear();
+    const ampm = hour >= 12 ? 'PM' : 'AM'
+
+    timeElement.innerHTML = hoursIn12HrFormat + ":" + minutes + `<span id="am-pm">${ampm}</span>`;
+    dateElement.innerHTML = days[day] + "," + " " + date + " " + months[month];
+    yearElement.innerHTML = year;
+
+},1)
 /*let cityInput = "Ankara";
 for (let i = 0; i < cityElement.length; i++) {
     const city = cityElement[i];
@@ -47,40 +68,58 @@ formElement.addEventListener('submit', (e) => {
 
 
 
-function fetchWeatherData() {
-    fetch(`http://api.weatherapi.com/v1/forecast.json?key=fab46e445d4c4a9eadd121055231003&q=Ankara&days=7&aqi=no&alerts=no`)
-    .then(response => response.json())
+function getWeatherData() {
+    navigator.geolocation.getCurrentPosition((success) => {
+     let {latitude, longitude } = success.coords;
+        fetch(`http://api.weatherapi.com/v1/future.json?key=c1d6ca2aaeed495fbaf131826231003&q=Ankara&dt=2023-04-09`)
+        .then(response => response.json())
 
-    .then(data => console.log(data))
-    .catch(err => console.log(err)); 
-    showWeatherData() ;  
-    
+        .then(data =>{
+
+            console.log(data)
+            showWeatherData() ;  
+        })
+        
+        
+    })
 }
 
 function showWeatherData(data){
+    let{humidity, pressure, sunrise, sunset, wind_speed} = data.current;
+    timeDayElement.innerHTML = data.timeDayElement;
+    countryElement.innerHTML =data.lat + 'N ' + data.lon+'E';
 
+    weatherItemsElement.innerHTML= `
+    
+    <div class="weather-items" id="weather-items">
+        <div>Nem</div>
+        <div>${humidity}%</div>
+    </div>
+
+    <div class="weather-items" id="weather-items">
+        <div>Basınç</div>
+         <div>${pressure}</div>
+    </div>
+
+    <div class="weather-items" id="weather-items">
+        <div>Rüzgar Hızı</div>
+         <div>${wind_speed}</div>
+    </div>
+
+    <div class="weather-items" id="weather-items">
+        <div>Güneş Doğuşu</div>
+        <div>${window.moment(sunrise * 1000).format('HH:mm a')}</div>
+    </div>
+
+    <div class="weather-items" id="weather-items">
+        <div>Güneş Batışı</div>
+        <div>${window.moment(sunset*1000).format('HH:mm a')}</div>
+    </div>
+
+    `;
+    
 }
-
-fetchWeatherData()
-const days = ["Pazar", "Pazartesi", "Salı", "Çarşamba", "Perşembe", "Cuma", "Cumartesi"];
-const months = ["Ocak", "Şubat", "Mart", " Nisan", "Mayıs", "Haziran", "Temmuz", "Ağustos","Eylül", "Ekim", "Kasım", "Aralık"];
-
-setInterval(() => {
-    const time = new Date();
-    const month = time.getMonth();
-    const date = time.getDate();
-    const day = time.getDay();
-    const hour = time.getHours();
-    const hoursIn12HrFormat = hour >= 13 ? hour % 12 : hour;
-    const minutes = time.getMinutes();
-    const year = time.getFullYear();
-    const ampm = hour >= 12 ? 'PM' : 'AM'
-
-    timeElement.innerHTML = hoursIn12HrFormat + ":" + minutes + `<span id="am-pm">${ampm}</span>`;
-    dateElement.innerHTML = days[day] + "," + " " + date + " " + months[month];
-    yearElement.innerHTML = year;
-
-},1)
+getWeatherData()
 
 
 
